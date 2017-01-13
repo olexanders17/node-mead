bb87fdd6c5cc36ce11238bf8a58f217488ca071d
olexanders17 <olexanders17@gmail.com>
2017-01-13 16:03:32 +0200

    initial commit

--- config/config.js
+++ config/config.js
@@ -1,10 +1,7 @@
-
-
-
 var env = process.env.NODE_ENV || "development";
 if (env === "development") {
     process.env.PORT = 3000;
-    process.env.MONGO_URI = "mongodb://localhost:27017/TodoApp"
+    process.env.MONGO_URI = "mongodb://heroku_6r1zxd7q:vmcgh192kv5qb8qg71hc37s9cp@ds163718.mlab.com:63718/heroku_6r1zxd7q" || "mongodb://localhost:27017/TodoApp";
 } else if (env === "test") {
     process.env.PORT = 3000;
     process.env.MONGO_URI = "mongodb://localhost:27017/TodoAppTest"
